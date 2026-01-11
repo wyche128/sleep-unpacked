95b519b23c71dfda3c83e8930e363f615888c8ce
wyche128 <ewyche@gmail.com>
2026-01-10 19:33:07 -0500

    Add Google Tag Manager to all pages

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Inter, Merriweather } from 'next/font/google';
+import Script from 'next/script';
 import './globals.css';
 import ScrollToTop from '../components/ScrollToTop';
 import Footer from '../components/Footer';
@@ -19,7 +20,28 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en" suppressHydrationWarning>
+            <head>
+                {/* Google Tag Manager */}
+                <Script id="google-tag-manager" strategy="afterInteractive">
+                    {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','GTM-N2SQ4TKF');`}
+                </Script>
+                {/* End Google Tag Manager */}
+            </head>
             <body className={`${inter.variable} ${merriweather.variable} font-sans text-graphite bg-white min-h-screen`}>
+                {/* Google Tag Manager (noscript) */}
+                <noscript>
+                    <iframe
+                        src="https://www.googletagmanager.com/ns.html?id=GTM-N2SQ4TKF"
+                        height="0"
+                        width="0"
+                        style={{ display: 'none', visibility: 'hidden' }}
+                    />
+                </noscript>
+                {/* End Google Tag Manager (noscript) */}
                 <ScrollToTop />
                 {children}
                 <Footer />
