ff4596bb96ab038804acb5e0da5af8920ec20755
wyche128 <ewyche@gmail.com>
2026-01-10 19:37:54 -0500

    Fix GTM to use dangerouslySetInnerHTML for reliable injection on all pages

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Inter, Merriweather } from 'next/font/google';
-import Script from 'next/script';
 import './globals.css';
 import ScrollToTop from '../components/ScrollToTop';
 import Footer from '../components/Footer';
@@ -21,14 +20,16 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en" suppressHydrationWarning>
             <head>
-                {/* Google Tag Manager */}
-                <Script id="google-tag-manager" strategy="afterInteractive">
-                    {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                    })(window,document,'script','dataLayer','GTM-N2SQ4TKF');`}
-                </Script>
+                {/* Google Tag Manager - using dangerouslySetInnerHTML for reliable head injection */}
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                        })(window,document,'script','dataLayer','GTM-N2SQ4TKF');`,
+                    }}
+                />
                 {/* End Google Tag Manager */}
             </head>
             <body className={`${inter.variable} ${merriweather.variable} font-sans text-graphite bg-white min-h-screen`}>
